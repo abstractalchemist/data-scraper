2de0344199dafce7c6ac10171ba6bbb5e32a0218
Jason Hirata <jhirata@integrity-apps.com>
2017-10-08 11:26:06 -1000

    added text processing

--- test/text_processing_text.js
+++ test/text_processing_text.js
@@ -10,7 +10,7 @@ describe('text processing', function() {
 	    "Color: Yellow   Side: Weiss  Character\n" +
 	    "Level: 1   Cost: 0   Power: 3000   Soul: 1\n" +
 	    "Trait 1: 使い魔 (Familiar)      Trait 2: 動物 (Animal)\n" +
-	    "Triggers: None" +
+	    "Triggers: None\n" +
 	    "Flavor: Fate: \"Thank you...... Arf......\"\n" +
 	    "TEXT: [A] BOND/\"Fate, Thinking of Mother\" [Put the top card of your Library in\n" +
 	    "your Clock]\n" +
@@ -19,10 +19,25 @@ describe('text processing', function() {
 	    "choose an \"Arf, Wolf Form\" in your Waiting Room and put it in the Slot this was\n" +
 	    "in.";
 	let obj = parsePartition(data);
+	expect(obj.data).to.eql(["Arf, Equal Partner",
+				 "対等のパートナー アルフ",
+				 "Card No.: N1/W32-002  Rarity: U",
+				 "Color: Yellow   Side: Weiss  Character",
+				 "Level: 1   Cost: 0   Power: 3000   Soul: 1",
+				 "Trait 1: 使い魔 (Familiar)      Trait 2: 動物 (Animal)",
+				 "Triggers: None",
+				 "Flavor: Fate: \"Thank you...... Arf......\"",
+				 "TEXT: [A] BOND/\"Fate, Thinking of Mother\" [Put the top card of your Library in",
+				 "your Clock]",
+				 "[A] CHANGE [(2) Discard a card from hand to your Waiting Room, put this in the",
+				 "Waiting Room] At the start of your Climax Phase, you may pay cost. If so,",
+				 "choose an \"Arf, Wolf Form\" in your Waiting Room and put it in the Slot this was",
+				 "in."])
 	expect(obj.lvl).to.equal("1");
 	expect(obj.id[0]).to.eql("Card No.: N1/W32-002  Rarity: U");
 	expect(obj.couchdbid).to.equal("n1-w32-002");
 	expect(obj.abilities).to.eql(["[A] BOND/\"Fate, Thinking of Mother\" [Put the top card of your Library in your Clock]", "[A] CHANGE [(2) Discard a card from hand to your Waiting Room, put this in the Waiting Room] At the start of your Climax Phase, you may pay cost. If so, choose an \"Arf, Wolf Form\" in your Waiting Room and put it in the Slot this was in."]);
+	expect(obj.splitid).to.eql(["n1","w32","002"]);
     })
     it('find image href', function() {
 	const dom = new JSDOM("<div/><div/><div class='card_details'><img class='thumbnail' src='foo'/></div>");
